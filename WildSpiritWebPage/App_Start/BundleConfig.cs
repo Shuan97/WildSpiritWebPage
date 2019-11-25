a814c900af690fa4800d75da8d0da5d1f3175651
Shuan <matura.jarek@gmail.com>
2019-11-25 18:33:01 +0100

    small fixes

--- WildSpiritWebPage/App_Start/BundleConfig.cs
+++ WildSpiritWebPage/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace WildSpiritWebPage
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/mynavbar.css"));
+                      "~/Content/site.css"));
         }
     }
 }
